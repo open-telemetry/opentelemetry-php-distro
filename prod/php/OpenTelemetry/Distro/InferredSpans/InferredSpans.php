e201aff294260fc76a55dfe49475f7bbd3656f2c
Pawel Filipczak <pawel.filipczak@elastic.co>
2026-04-23 21:00:19 +0200

    fix: internal inferred spans filtering for scoped namespace

--- prod/php/OpenTelemetry/Distro/InferredSpans/InferredSpans.php
+++ prod/php/OpenTelemetry/Distro/InferredSpans/InferredSpans.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Distro\InferredSpans;
 
-use OpenTelemetry\Distro\ScoperConfig;
+use OpenTelemetry\Distro\OTelDistroScoperConfig;
 use OpenTelemetry\Distro\Util\ArrayUtil;
 use OpenTelemetry\API\Globals;
 use OpenTelemetry\API\Behavior\LogsMessagesTrait;
@@ -185,7 +185,7 @@ class InferredSpans
             $frame = $stackTrace[$index];
             if (
                 array_key_exists('class', $frame) &&
-                (str_starts_with($frame['class'], ScoperConfig::PREFIX) || str_starts_with($frame['class'], 'OpenTelemetry\\')) // TODO allow namespaces to be configured
+                (str_starts_with((string)$frame['class'], OTelDistroScoperConfig::PREFIX) || str_starts_with((string)$frame['class'], 'OpenTelemetry\\')) // TODO allow namespaces to be configured
             ) {
                 $cutIndex = $index;
                 break;
